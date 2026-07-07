dc463f14043cc36d81ae7c74fa35ddad0e4eaa0d
Marcos Rossano Angeloni <47757769+MarcosAng@users.noreply.github.com>
2026-07-06 21:45:20 -0300

    arquivos da aula 1.1

---
+++ carregando_csv.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+df = pd.read_csv('C:\\cursos\\sctec\\analisedados\\primeiro_projeto_python\\1.1\\dados.csv')
+
+#imprime todos os dados da planilha
+print(df)
+
+#cabecalho do dataframe
+print(df.head())
